999aecbc542767c7bad69bbc4c1aa9f45499f68e
anks100992 <arpitnigam100992@gmail.com>
2021-01-09 00:31:06 +0530

    inside src/test/java added package named as runner and inside that added a java file named TestRunner.java

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,23 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features="classpath:features",//to tell cucumber where is ur feature file
+        glue="stepdefs", // to tell cucumber where is ur step def code
+        tags="", // to tell which tagged feature file to execute
+        plugin = {"pretty", // to generate reports
+            "html:target/html/",
+            "json:target/json/file.json",
+                  },
+        //publish=true,
+        dryRun=false // to tell whether to test run(true) or actual run(false)
+        )
+
+public class TestRunner {
+
+}
